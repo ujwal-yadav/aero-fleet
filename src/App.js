2f57f2f6279700da79266f86ae1d2a50a250df8d
ujwal-yadav <ujwalofficial99@gmail.com>
2024-08-06 22:41:50 +0530

    feat: add root app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { Toaster } from './components/ui/toaster';
+import { AuthProvider } from './context/AuthProvider';
+import { ThemeProvider } from './context/ThemeProvider';
+import { ProtectedRoute } from './layout/ProtectedRoute';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DroneDetails } from './pages/DroneDetails/DroneDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <div className="flex flex-col justify-center w-full min-h-screen bg-background">
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/drones/:id" element={<DroneDetails />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+      <Toaster />
+    </ThemeProvider>
   );
 }
 
